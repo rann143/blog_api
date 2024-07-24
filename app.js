f18a2aed4fb91d3f34ef874d2a4c97a1e81f57d5
Daniel <roderm99@gmail.com>
2024-07-24 16:39:11 -0400

    Change name of mongo env variable

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ main().catch((err) => {
   console.log(err);
 });
 async function main() {
-  await mongoose.connect(process.env.MONGO_DB);
+  await mongoose.connect(process.env.MONGO_URI);
 }
 
 app.use(compression());
